0f7cee8bce5fee34440f3b4387f26be77cda67cc
“Shivam <shivam6132@gmail.com>
2026-01-09 08:59:53 -0800

    Refactor server listening logic to use environment variable for port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -345,7 +345,9 @@ app.get("/api/v1/getuserinfo", userMiddleware, async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("server running succesfull");
-  connectDb();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("server running on port", PORT);
 });
+
